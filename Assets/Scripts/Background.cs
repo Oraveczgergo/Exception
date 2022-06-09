5d7396dd2bc10dc88c0392cd6f5efe26ca40d4d3
Oraveczgergo <90648114+Oraveczgergo@users.noreply.github.com>
2022-06-09 19:23:54 +0200

    Background Repeat

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -8,17 +8,33 @@ public class Background : MonoBehaviour
     public float yEffect;
     public GameObject followedObject;
     private float length;
-    private Vector3 startPos;
+    private Vector2 startPos;
     void Start()
     {
-        startPos = transform.position;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
+        GameObject bg01 = GameObject.Find("BG_01");
+        if (name == "BG_02")
+        {
+            transform.position = bg01.transform.position + Vector3.right * length;
+        }
+        else if (name == "BG_03")
+        {
+            transform.position = bg01.transform.position + Vector3.right * length * 2;
+        }
+        startPos = transform.position;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        float xDistance = followedObject.transform.position.x * xEffect; 
+        float xDistance = followedObject.transform.position.x * xEffect;
         float yDistance = followedObject.transform.position.y * yEffect;
-        transform.position = new Vector3(startPos.x + xDistance, startPos.y + yDistance, transform.position.z);
+        float diffx = followedObject.transform.position.x - (startPos.x + xDistance);
+        //if (name == "BG_01")           
+            //Debug.Log(followedObject.name + " : " + diffx + " == " + (followedObject.transform.position.x - (startPos.x + xDistance)) + " ("+ followedObject.transform.position.x + " - " + (startPos.x + xDistance) + ")" + " length: " + length * 1.5f);
+        if (diffx > length * 1.5f)
+        {
+            startPos += new Vector2(length * 3, 0);
+        }
+        transform.position = new Vector2(startPos.x + xDistance, startPos.y + yDistance);
     }
 }
